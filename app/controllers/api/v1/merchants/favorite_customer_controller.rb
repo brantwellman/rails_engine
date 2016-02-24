2c739c893e164b0df182b0b8109845ef26cd8ed9
Brant Wellman <BrantWellman@gmail.com>
2016-02-24 15:54:06 -0700

    Add returns customer with most transactions for merchant

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,15 +1,15 @@
-class Api::V1::Customers::FavoriteMerchantController < ApplicationController
+class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
   respond_to :json
 
   def show
-    frequencies = Customer.find(params[:id]).
+    frequencies = Merchant.find(params[:id]).
                            invoices.joins(:transactions).
                            where(transactions: { result: "success"}).
-                           group(:merchant_id).count
+                           group(:customer_id).count
 
     merchant_id = frequencies.max_by { |k,v| v }.first
 
-    respond_with Merchant.find(merchant_id)
+    respond_with Customer.find(merchant_id)
   end
 
 end
