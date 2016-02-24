8ae281a1cf59f6534b50c132794318441d326a68
Brant Wellman <BrantWellman@gmail.com>
2016-02-24 16:55:03 -0700

    Add merchant revenue by date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,14 +2,22 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   respond_to :json
 
   def show
-    result = Merchant.find(params[:id]).invoices.
-                              joins(:transactions).
-                              where(transactions: { result: "success" }).
-                              joins(:invoice_items).
-                              sum("quantity * unit_price")
+    if params[:date]
+      result = Merchant.find(params[:id]).invoices.
+                                where(invoices: { created_at: params[:date]}).
+                                joins(:transactions).
+                                where(transactions: { result: "success" }).
+                                joins(:invoice_items).
+                                sum("quantity * unit_price")
+      respond_with({revenue: result})
+    else
+      result = Merchant.find(params[:id]).invoices.
+                                joins(:transactions).
+                                where(transactions: { result: "success" }).
+                                joins(:invoice_items).
+                                sum("quantity * unit_price")
 
-    respond_with({revenue: result})
+      respond_with({revenue: result})
+    end
   end
-
-
 end
