d38a74587202a81a8bb43d4347d1736787feb8f2
Brant Wellman <BrantWellman@gmail.com>
2016-02-24 21:18:31 -0700

    Refactor transactions finders controller

--- app/controllers/api/v1/transactions/finders_controller.rb
+++ app/controllers/api/v1/transactions/finders_controller.rb
@@ -2,48 +2,35 @@ class Api::V1::Transactions::FindersController < ApplicationController
   respond_to :json
 
   def show
-    find_params
+    transactions = Transaction.where(transaction_params)
+    unless transaction_name_params.empty?
+      respond_with transactions.find_by(build_query)
+    else
+      respond_with transactions.first
+    end
   end
 
   def index
-    find_all_params
+    transactions = Transaction.where(transaction_params)
+    unless transaction_name_params.empty?
+      respond_with transactions.where(build_query)
+    else
+      respond_with transactions
+    end
   end
 
   private
 
-    def find_params
-      if params[:id]
-        respond_with Transaction.find(params[:id])
-      elsif params[:result]
-        respond_with Transaction.find_by('LOWER(result) = ?', params[:result].downcase)
-      elsif params[:invoice_id]
-        respond_with Transaction.find_by(invoice_id: params[:invoice_id])
-      elsif params[:credit_card_number]
-        respond_with Transaction.find_by(credit_card_number: params[:credit_card_number])
-      elsif params[:credit_card_expiration_date]
-        respond_with Transaction.find_by(credit_card_expiration_date: params[:credit_card_expiration_date])
-      elsif params[:created_at]
-        respond_with Transaction.find_by(created_at: params[:created_at])
-      elsif params[:updated_at]
-        respond_with Transaction.find_by(updated_at: params[:updated_at])
-      end
+    def build_query
+      query = []
+      query << "result ILIKE '#{transaction_name_params[:result]}'" if transaction_name_params[:result]
     end
 
-    def find_all_params
-      if params[:id]
-        respond_with Transaction.where(id: params[:id])
-      elsif params[:result]
-        respond_with Transaction.where('LOWER(result) = ?', params[:result].downcase)
-      elsif params[:invoice_id]
-        respond_with Transaction.where(invoice_id: params[:invoice_id])
-      elsif params[:credit_card_number]
-        respond_with Transaction.where(credit_card_number: params[:credit_card_number])
-      elsif params[:credit_card_expiration_date]
-        respond_with Transaction.where(credit_card_expiration_date: params[:credit_card_expiration_date])
-      elsif params[:created_at]
-        respond_with Transaction.where(created_at: params[:created_at])
-      elsif params[:updated_at]
-        respond_with Transaction.where(updated_at: params[:updated_at])
-      end
+    def transaction_params
+      params.permit(:id, :invoice_id, :credit_card_number, :created_at, :updated_at)
+    end
+
+    def transaction_name_params
+      params.permit(:result)
     end
 end
